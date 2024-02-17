11e961819be282c6fe1b68732e3a81fd4959ed7a
BreGurney <150399227+BreGurney@users.noreply.github.com>
2024-02-16 16:53:31 -0800

    Functions
    Here is the basic function of operators using JavaScript

---
+++ Projects2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function myFunction() {
+    var sentence = "I live in Bridge River";
+    sentence +=" outside of Lillooet BC";
+    document.getElementById("Concatenate") .innerHTML = sentence;
+}
