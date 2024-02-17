5280374bca7f144331418cc80e0c1eacd0c82bf1
BreGurney <150399227+BreGurney@users.noreply.github.com>
2024-02-16 16:10:54 -0800

    Assignment
    Here is the Basic JavaScript Projects Assignment

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,12 @@
+//Window alert
+window.alert("Hello, World!");
+
+//Two variables
+var Sent1 = "This is the beginning of the string";
+var Sent2 = " and this is the end of the string";
+
+//Concatenate 
+document.write(Sent1 + Sent2);
+
+//an expression
+document.write(56*24);
