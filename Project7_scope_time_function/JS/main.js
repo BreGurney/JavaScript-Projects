077a1667ec70beb96aeb88e78d085ec70213d01e
BreGurney <150399227+BreGurney@users.noreply.github.com>
2024-02-22 17:26:58 -0800

    Scope Time Functions
    Global/Local variable
    Time Functions

---
+++ Project7_scope_time_function/JS/main.js
@@ -0,0 +1,33 @@
+
+function Add_numbers_1() {
+    var X = 10;
+    document.write(20 + X + "<br>");
+}
+function Add_numbers_2() {
+    document.write(X + 100);
+}
+Add_numbers_1();
+Add_numbers_2();
+
+function get_Date() {
+    if (new Date().getHours() < 18) {
+        document.getElementById("Greeting").innerHTML = "How are you today?";
+    }
+}
+
+function demo() {
+    if (new Date().getHours() < 18) {
+        document.getElementById("Here").innerHTML = "Home Time."
+    }
+}
+
+function Age_Function() {
+    Age = document.getElementById("Age").value
+    if (Age >= 18) {
+        Vote = "You are old enough to vote!";
+    }
+    else {
+        Vote = "You are not old enough to vote!";
+    }
+    document.getElementById("How_old_are_you?").innerHTML = Vote;
+}
