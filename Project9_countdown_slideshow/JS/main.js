2d0b01baf3b7ff16d32dc43f06ba3dec0acc6a77
BreGurney <150399227+BreGurney@users.noreply.github.com>
2024-02-26 12:24:23 -0800

    Update main.js
    Fixed typos

--- Project9_countdown_slideshow/JS/main.js
+++ Project9_countdown_slideshow/JS/main.js
@@ -13,12 +13,12 @@ function showSlides(n) {
     let i;
     let slides = document.getElementsByClassName("mySlides");
     let dots = document.getElementsByClassName("dot");
-    if (n > slides.legnth) {slideIndex = 1}
-    if (n < 1) {slideIndex = slide.legnth}
-    for (i = 0; 1 < slides.legnth; i ++) {slides[i].computedStyleMap.display = "none"};
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slide.length}
+    for (i = 0; 1 < slides.length; i ++) {slides[i].computedStyleMap.display = "none"};
     }
 
-    for (i = 0; i < dots.legnth; i++) {
+    for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace("active", "");
     }
     slides[slideIndex-1].style.display = "block";
